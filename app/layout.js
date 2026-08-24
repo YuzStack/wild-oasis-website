c1754cbc77429fd068be49925d3c2e6c55fa36e3
Yusuf Oyinlola <yuzstackinnovation@gmail.com>
2026-08-24 16:24:11 +0100

    improve the navigation and root layout

--- app/layout.js
+++ app/layout.js
@@ -1,14 +1,11 @@
-import Logo from '../components/Logo';
-import Navigation from '../components/Navigation';
 import '../styles/globals.css';
 import { Josefin_Sans } from 'next/font/google';
+import Header from '@/components/Header';
 
 const josefin = Josefin_Sans({
   subsets: ['latin'],
   display: 'swap',
 });
-
-console.log(josefin);
 
 export const metadata = {
   title: {
@@ -22,13 +19,15 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang='en'>
-      <body className={`bg-primary-950 text-primary-100 min-h-screen ${josefin.className}`} >
+      <body
+        className={`min-h-screen bg-primary-950 text-primary-100 antialiased ${josefin.className} flex flex-col`}
+      >
         <header>
-          <Logo />
-          <Navigation />
+          <Header />
         </header>
-        <main>{children}</main>
-        <footer>Copyright by The Wild Oasis</footer>
+        <div class='flex-1 px-8 py-12'>
+          <main className='mx-auto max-w-7xl'>{children}</main>
+        </div>
       </body>
     </html>
   );
