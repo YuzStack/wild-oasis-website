e0754dd184439889327c763447042a300569fe04
Yusuf Oyinlola <yuzstackinnovation@gmail.com>
2026-08-24 16:00:15 +0100

    add custom tailwind utility classes, page metadata, favicon, and font

--- app/layout.js
+++ app/layout.js
@@ -1,15 +1,28 @@
 import Logo from '../components/Logo';
 import Navigation from '../components/Navigation';
-// import '../styles/globals.css';
+import '../styles/globals.css';
+import { Josefin_Sans } from 'next/font/google';
+
+const josefin = Josefin_Sans({
+  subsets: ['latin'],
+  display: 'swap',
+});
+
+console.log(josefin);
 
 export const metadata = {
-  title: 'The Wild Oasis Website',
+  title: {
+    default: 'Welcome | The Wild Oasis',
+    template: '%s | The Wild Oasis',
+  },
+  description:
+    'Luxurious cabin hotel, located in the heart of the Italian Dolomites, surrounded by beautiful mountains and dark forests.',
 };
 
 export default function RootLayout({ children }) {
   return (
     <html lang='en'>
-      <body>
+      <body className={`bg-primary-950 text-primary-100 min-h-screen ${josefin.className}`} >
         <header>
           <Logo />
           <Navigation />
